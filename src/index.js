8602923d8825d6633a51688acdd406ded53a2b44
KatieCodeWitch <71100992+katiekate88@users.noreply.github.com>
2020-10-29 18:14:44 -0400

    Added forecast functionality

--- src/index.js
+++ src/index.js
@@ -20,6 +20,20 @@ let todayDate = date.getDate();
 return `${day}, ${month} ${todayDate}</br>${hours}:${minutes} ET`;
 }
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 
 function showWeather(response){
 document.querySelector("#city-result").innerHTML = response.data.name;
@@ -37,9 +51,45 @@ celsiusTempLow = response.data.main.temp_min;
 
 }
 
+function showForecast(response){
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col">
+     <div class="card" >
+        <div class="card-body">
+      <h5 class = "card-title">
+        ${formatHours(forecast.dt * 1000)}
+      </h5>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong id = "forecast-high">
+          ${Math.round(forecast.main.temp_max)}°
+        </strong> | <span id = "forecast-low">
+        ${Math.round(forecast.main.temp_min)}° 
+        </span>
+      </div>
+    </div>
+      </div>
+    </div>
+  `;
+  }
+}
+
 function search(city) {
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(showWeather); 
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(showForecast);
 }
 
 function handleSubmit(event) {
